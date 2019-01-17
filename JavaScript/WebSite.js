63a74c4561c0013c903a614b37230786e5d870cc
Parsa Yadollahi <parsayadollahi@gmail.com>
2019-01-16 21:17:41 -0500

    CourseTalk btn buzz

--- JavaScript/WebSite.js
+++ JavaScript/WebSite.js
@@ -17,7 +17,7 @@ function myFunction() {
     btnText.innerHTML = 'More... <i class="fa fa-angle-double-right"></i>';
     moreText.style.display = 'none';
   } else {
-    btnText.innerHTML = '<i class="fa fa-angle-double-left"></i> Less';
+    btnText.innerHTML = '<i class="fa fa-angle-double-left"></i>Less';
     moreText.style.display = 'inline';
   }
 }
