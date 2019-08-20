100898071f3c95879e14da4465bad9ff4bf4df0f
Parsa Yadollahi <payadoll@cisco.com>
2019-08-20 12:33:57 -0400

    added smooth ahref back

--- JavaScript/WebSite.js
+++ JavaScript/WebSite.js
@@ -39,6 +39,16 @@ function dateAndTime() {
 dateAndTime();
 
 // Smooth scrolling
+$(document).on('click', 'a[href^="#"]', function (event) {
+  event.preventDefault();
+
+  $('html, body').animate({
+    scrollTop: $($.attr(this, 'href')).offset().top
+  }, 500);
+});
+
+
+// Animate on scroll
 AOS.init({
   // Global settings:
   disable: false, // accepts following values: 'phone', 'tablet', 'mobile', boolean, expression or function
