aa39e485bb65e6bca0c57410b894aa0aaf5979bc
Parsa Yadollahi <parsayadollahi@gmail.com>
2019-10-04 19:11:56 -0400

    adjusted project section - support more projs

--- JavaScript/WebSite.js
+++ JavaScript/WebSite.js
@@ -71,3 +71,8 @@ AOS.init({
   anchorPlacement: 'top-bottom', // defines which position of the element regarding to window should trigger the animation
 
 });
+
+// MDB Lightbox Init
+$(function () {
+  $("#mdb-lightbox-ui").load("mdb-addons/mdb-lightbox-ui.html");
+  });
